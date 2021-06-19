54821d4477af6117883bc1480109c3fc69baa624
Seo-Hongju <aayy4321@naver.com>
2021-06-19 13:18:53 +0900

    Day1과제

--- 차준규/src/Day01/Day01_과제.java
+++ 차준규/src/Day01/Day01_과제.java
@@ -23,7 +23,11 @@ public class Day01_°úÁ¦ {
 	  ||_/=\\__|
 	  
 	 */
-	
+	public static void main(String[] args) {
+		System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+		System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+		
+	}
 	
 	
 	
