2eb1059cedfd850c25e72d5d8035d21944d5b09e
차준규 <aayy4321@naver.com>
2021-06-26 09:55:04 +0900

    Day3

---
+++ 차준규/src/Day03/Day03_1.java
@@ -0,0 +1,27 @@
+package Day03;
+
+public class Day03_1 {
+	
+	// 1. 출력
+		// print , printf , println 
+		//  제어문자 : \n \t \r(개행 ==> 맨앞으로 가게 해주는 기능)
+		// 형식문자 : %d %c %f %s
+	//2. 연산자
+		// 산술연산자 : + - * / %(나머지값)
+		// 비교연산자 : > < >= <= == !=
+		// 논리연산자 : && 		|| 		!
+		// 대입연산자 : =(오른쪽에서 왼쪽으로 += 등 
+		// 증감연산자 : 변수++ 변수-- ++변수 --변수 
+	// 3. 변수 [ 메모리 ]
+		// 자료형 : 미리 정해진 메모리 크기 
+			// boolean , byte , char , short , int , long , float , double 등 
+		// 변수 주소값 : 컴퓨터의 변수를 찾는 위치 값 [16진수]
+	// 4. 입력 받기 [ 입력 객체 ] 
+		//Scanner 클래스 -> import 를 해야한다. 
+			// Scanner 객체명 = new Scanner(System.in);
+		// 입력객체에서 데이터 가져오기 => .next()
+	public static void main(String[] args) {
+		
+	}
+
+}
